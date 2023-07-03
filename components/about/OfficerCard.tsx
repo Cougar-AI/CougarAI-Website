9d3c67bf22cba1f1eeff6abed591b5c95265152e
tachorzy <tachorzyad@gmail.com>
2023-07-02 22:32:53 -0500

    Added SocialLink component

--- components/about/OfficerCard.tsx
+++ components/about/OfficerCard.tsx
@@ -1,16 +1,22 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import SocialLink from '@/components/about/SocialLink'
 import { satoshi, satoshiBold, satoshiMedium } from '@/util/localFonts';
+
+interface officerCardProps{
+    name: string;
+    officerRole: string;
+}
+
+
 
 const OfficerCard = () => {
     return(
-        <div className="flex flex-col relative bg-snow rounded-3xl h-80 w-[18.5rem] items-center p-8 overflow-hidden">
+        <div className="flex flex-col relative bg-snow rounded-md h-80 w-[18.5rem] items-center p-8 overflow-hidden">
             <Image src="/Icons/placeholder_officer_headshot.svg" width={100} height={100} className="rounded-full pb-2 z-50" alt="Officer headshot"></Image>
             <h1 className="text-cai-500 text-2xl text-center pt-2 z-50">Zeel Engineer</h1>
             <h2 className="text-cai-500 text-xl text-center pb-2 z-50">President</h2>
-            <Link href="https://www.linkedin.com/in/testing-CAI/" className="pt-1 pb-2 z-50">
-                <Image src="/Icons/LinkedIn_icon.svg" width={40} height={40} className="z-50 hover:scale-105 hover:rotate-2 duration-700" alt="LinkedIn Icon"></Image>
-            </Link>
+            <SocialLink platform="LinkedIn" accountLink={'https://www.linkedin.com/'} platformIcon='/Icons/LinkedIn_icon.svg'></SocialLink>
             <Image src="/Icons/node-network-test.png" width={1000} height={"0"} className="absolute h-[115%] scale-[80%] rounded-tr-3xl top-0 ml-[10rem] mt-4 object-none z-0" alt="Card Pattern"></Image>
         </div>
     )
---
+++ components/about/SocialLink.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+interface socialLinkProps{
+  platform: string;
+  accountLink: string;
+  platformIcon: string;
+  //for now the only social link we have is LinkedIn which will be 40x40 if we add another social in the future the dimensions may be diff for that one.
+  // iconWidth: number;
+  // iconHeight: number;
+}
+
+const SocialLink = (props: socialLinkProps) => {
+    return(
+      <Link href={`${props.accountLink}`} className="pt-1 pb-2 z-50" target="_blink">
+      <Image src={`${props.platformIcon}`} width={40} height={40} className="z-50 hover:scale-105 hover:rotate-2 duration-700" alt={`${props.platform} icon`}></Image>
+  </Link>
+    );
+}
+
+export default SocialLink;
