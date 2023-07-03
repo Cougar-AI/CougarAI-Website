f02d69094c95a72b8a6395adf00b2bc493c963a3
tachorzy <tachorzyad@gmail.com>
2023-07-03 04:44:14 -0500

    Added flexbox to OfficerCardTray

--- components/about/OfficerCardTray.tsx
+++ components/about/OfficerCardTray.tsx
@@ -3,7 +3,7 @@ import { officers } from '@/util/officerInformation/officers'
 
 const OfficerCardTray = () => {
     return(
-        <div>
+        <div className="flex flex-col md:max-2xl:flex-row flex-wrap gap-y-4 md:max-2xl:gap-y-6 md:max-2xl:gap-x-6">
          {officers.map(([officerRole, officerName, officerLinkedIn], index) => (
             <OfficerCard name={officerName} officerRole={officerRole} officerLinkedIn={officerLinkedIn}></OfficerCard>
          ))}
