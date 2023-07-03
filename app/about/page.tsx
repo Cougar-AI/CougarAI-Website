42c7bd9936d4bda150dc8a6f6394f0b86a9ee4d1
tachorzy <tachorzyad@gmail.com>
2023-07-03 03:20:27 -0500

    Added mobile responsiveness to Officer section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ import OfficerCard from '@/components/about/OfficerCard'
 
 export default function About() {
     return (
-      <main className="bg-cai-400 min-h-screen md:max-2xl:grid lg:max-2xl:grid-cols-2 ">
+      <main className="bg-cai-400 min-h-screen flex flex-col md:max-2xl:grid lg:max-2xl:grid-cols-2">
         {/* one column in the grid */}
         <div>
           {/* the padding top (for lg screens and up) here might need a tweak to be consistent with the padding elsewhere on the page */}
@@ -25,13 +25,13 @@ export default function About() {
               </div>
           </section>
 
-          <section className= {satoshiBold.className + " md:max-2xl:pt-36 md:max-2xl:pl-32  md:max-2xl:pb-16"}>
-              <h1 className="md:max-2xl:pt-6 md:max-2xl:pb-6 align-bottom text-5xl">The Officers</h1>
+          <section className= {satoshiBold.className + " flex flex-col text-center items-center md:max-2xl:pt-36 md:max-2xl:pl-32  md:max-2xl:pb-16"}>
+              <h1 className="pt-6 pb-2.5 md:max-2xl:py-6 align-bottom md:max-2xl:text-5xl text-3xl">The Officers</h1>
               <OfficerCard></OfficerCard>
           </section>
         </div>
         <div>
-          <Image src="/Icons/binary.svg" width="0" height="0" className="absolute right-0 select-none overflow-hidden object-contain lg:max-2xl:object-fill w-full h-[50%] mt-36 lg:max-2xl:w-1/2 lg:max-2xl:h-[47%] md:h-[70%] lg:max-2xl:pl-72 md:top-0 md:mt-[50rem] lg:max-2xl:mt-[30.5rem]" alt="Binary Pattern"></Image>
+          <Image src="/Icons/binary.svg" width="0" height="0" className="md:max-2xl:absolute right-0 select-none overflow-hidden object-contain lg:max-2xl:object-fill w-full h-[50%] mt-36 lg:max-2xl:w-1/2 lg:max-2xl:h-[47%] md:h-[70%] lg:max-2xl:pl-72 md:top-0 md:mt-[50rem] lg:max-2xl:mt-[30.5rem]" alt="Binary Pattern"></Image>
         </div>
       </main>
     )
--- components/about/OfficerCard.tsx
+++ components/about/OfficerCard.tsx
@@ -10,14 +10,14 @@ interface officerCardProps{
 
 const OfficerCard = () => {
     return(
-        <div className="flex flex-col relative content-end bg-snow rounded-md h-64 w-52 md:max-2xl:h-[19rem] md:max-2xl:w-60 items-center md:max-2xl:p-8 p-2 overflow-hidden">
-            <Image src="/Icons/placeholder.png" width={130} height={130} className="rounded-full z-40 border-misty-rose border-8 scale-[80%] md:max-2xl:scale-100" alt="Officer headshot"></Image>
-            <div className="flex flex-col items-center md:max-2xl:pb-2 z-40">
-                <h1 className="text-cai-500 text-2xl text-center drop-shadow-sm md:max-2xl:pt-1.5">Zeel Engineer</h1>
-                <h2 className="text-cai-500 text-xl text-center drop-shadow-sm md:max-2xl:pb-2">President</h2>
+        <div className="flex flex-col relative gap-y-0 bg-snow rounded-md h-64 w-52 md:max-2xl:h-[19rem] md:max-2xl:w-60 items-center md:max-2xl:p-8 p-1.5 overflow-hidden">
+            <Image src="/Icons/placeholder.png" width={130} height={130} className="rounded-full z-40 border-misty-rose border-8 scale-[80%] md:max-2xl:scale-100 h/2" alt="Officer headshot"></Image>
+            <div className="flex flex-col items-center md:max-2xl:pb-2 z-40 h-1/2">
+                <h1 className="text-cai-500 text-2xl text-center drop-shadow-sm md:max-2xl:pt-1.5 -pt-1.5">Zeel Engineer</h1>
+                <h2 className="text-cai-500 text-xl text-center drop-shadow-sm md:max-2xl:pb-2 pb-1.5">President</h2>
                 <SocialLink platform="LinkedIn" accountLink={'https://www.linkedin.com/'} platformIcon='/Icons/LinkedIn_icon.svg'></SocialLink>
             </div>
-            <Image src="/Icons/neural-pattern-mistyrose.png" width={350} height={"0"} className="absolute h-[115%] scale-[175%] rounded-tr-3xl top-0 ml-28 mt-20 object-none z-0 select-none" alt="Card Pattern"></Image>
+            <Image src="/Icons/neural-pattern-mistyrose.png" width={350} height={"0"} className="absolute h-[115%] scale-[155%] md:max-2xl:scale-[180%] rounded-tr-3xl top-0 ml-28 md:max-2xl:mt-20 mt-16 object-none z-0 select-none" alt="Card Pattern"></Image>
         </div>
     )
 }
