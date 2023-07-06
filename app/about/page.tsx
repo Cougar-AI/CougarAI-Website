fb424314da29e1d49dccd334387413b49378af4f
tachorzy <tachorzyad@gmail.com>
2023-07-06 03:01:42 -0500

    Adjusted margins and spacing on mobile about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,7 +13,7 @@ export default function About() {
       <main className="bg-cai-400 min-h-screen flex flex-col md:max-2xl:grid lg:max-2xl:grid-cols-2">
         <section>
           {/* the padding top (for lg screens and up) here might need a tweak to be consistent with the padding elsewhere on the page */}
-          <div className={satoshiBold.className + " flex flex-col gap-y-24 text-center md:max-2xl:text-start px-5 md:max-2xl:px-0 md:max-2xl:items-start pt-20 md:max-2xl:pt-36 md:max-2xl:pl-32"}>
+          <div className={satoshiBold.className + " flex flex-col gap-y-10 lg:max-2xl:gap-y-24 text-center md:max-2xl:text-start px-5 md:max-2xl:px-0 md:max-2xl:items-start pt-20 md:pt-72 lg:max-2xl:pt-36 md:max-2xl:pl-32"}>
             {aboutUsInformation.map(([title, description, textSize], index) => (
               <div key = {index}>
                 <h1 className={`pb-2 md:max-2xl:pb-6 align-bottom ${textSize}`}>{title}</h1>
@@ -22,12 +22,12 @@ export default function About() {
             ))}
           </div>
           
-          <div className= {satoshiBold.className + " flex flex-col text-center md:max-2xl:text-start md:max-2xl:items-start items-center md:max-2xl:pt-36 md:max-2xl:mx-32 mr-64dd md:max-2xl:pb-16 w-screen"}>
-              <h1 className="pt-6 pb-2.5 md:max-2xl:py-6 align-bottom md:max-2xl:text-5xl text-3xl">The Officers</h1>
+          <div className= {satoshiBold.className + " flex flex-col text-center md:max-2xl:text-start md:max-2xl:items-start items-center my-10 lg:max-2xl:my-24 md:max-2xl:pt-36 md:max-2xl:mx-32 mr-64 md:max-2xl:pb-16 w-screen"}>
+              <h1 className="pt-6 pb-2.5 mb-2.5 md:max-2xl:mb-0 md:max-2xl:py-6 align-bottom md:max-2xl:text-5xl text-3xl">The Officers</h1>
               <OfficerCardTray/>
           </div>
         </section>
-        <Image src="/Icons/binary.svg" width="0" height="0" className="md:max-2xl:absolute right-0 select-none overflow-hidden object-contain lg:max-2xl:object-fill w-full h-[50%] mt-36 lg:max-2xl:w-1/2 lg:max-2xl:h-[47%] md:h-[70%] lg:max-2xl:pl-72 md:top-0 md:mt-[50rem] lg:max-2xl:mt-[30.5rem]" alt="Binary Pattern"></Image>
+        <Image src="/Icons/binary.svg" width="0" height="0" className="lg:max-2xl:absolute lg:max-2xl:right-0 select-none overflow-hidden object-contain lg:max-2xl:object-fill w-full h-[50%] mt-36 lg:max-2xl:w-1/2 lg:max-2xl:h-[47%] md:h-[70%] lg:max-2xl:pl-72 md:top-0 md:mt-[50rem] lg:max-2xl:mt-[30.5rem]" alt="Binary Pattern"></Image>
       </main>
     )
   }
--- components/about/OfficerCardTray.tsx
+++ components/about/OfficerCardTray.tsx
@@ -3,7 +3,7 @@ import { officers } from '@/util/officerInformation/officers'
 
 const OfficerCardTray = () => {
     return(
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:max-2xl:grid-cols-4 gap-y-4 md:max-2xl:gap-y-7 md:max-2xl:gap-x-7">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:max-2xl:grid-cols-4 gap-y-5 md:max-2xl:gap-y-7 md:max-2xl:gap-x-7">
          {officers.map(([officerRole, officerName, officerLinkedIn], index) => (
             <OfficerCard key={index} name={officerName} officerRole={officerRole} officerLinkedIn={officerLinkedIn}></OfficerCard>
          ))}
