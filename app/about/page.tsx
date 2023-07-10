413127f8e8484bf33dea31aa0c3ecc58bbf09bc1
tachorzy <tachorzyad@gmail.com>
2023-07-10 02:44:00 -0500

    Added info cards to about us page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,11 +9,13 @@ export default function About() {
       ["What we do.","CougarAI aims to educate members in the latest advances in artificial intelligence, data science, and machine learning. As well as to provide a space for networking among those interested in the field.", "text-3xl md:max-2xl:text-5xl"],
     ];
 
+    const aboutUsCardInformation: string[] = ["Workshops", "Info Sessions", "Social Events"]
+
     return (
       <main className="bg-cai-400 min-h-screen flex flex-col md:max-2xl:grid lg:max-2xl:grid-cols-2">
-        <section>
+        <section className="md:max-2xl:ml-32">
           {/* the padding top (for lg screens and up) here might need a tweak to be consistent with the padding elsewhere on the page */}
-          <div className={satoshiBold.className + " flex flex-col gap-y-10 lg:max-2xl:gap-y-24 text-white text-center md:max-2xl:text-start px-5 md:max-2xl:px-0 md:max-2xl:items-start pt-20 md:pt-72 lg:max-2xl:pt-36 md:max-2xl:pl-32"}>
+          <div className={satoshiBold.className + " flex flex-col gap-y-10 lg:max-2xl:gap-y-24 text-white text-center md:max-2xl:text-start px-5 md:max-2xl:px-0 md:max-2xl:items-start pt-20 md:pt-72 lg:max-2xl:pt-36 mb-3.5"}>
             {aboutUsInformation.map(([title, description, textSize], index) => (
               <div key = {index}>
                 <h1 className={`pb-2 md:max-2xl:pb-6 align-bottom ${textSize}`}>{title}</h1>
@@ -22,7 +24,16 @@ export default function About() {
             ))}
           </div>
           
-          <div className= {satoshiBold.className + " flex flex-col text-center md:max-2xl:text-start md:max-2xl:items-start items-center my-10 lg:max-2xl:my-24 md:max-2xl:pt-36 md:max-2xl:mx-32 mr-64 md:max-2xl:pb-16 w-screen"}>
+          <div className="grid grid-cols-2 gap-x-5 gap-y-5">
+            {aboutUsCardInformation.map((cardTitle, index) => (
+              <div key={index} className="relative flex flex-row gap-x-4 bg-snow rounded-md h-16 items-center overflow-hidden">
+                <Image src="/Icons/chip_placeholder.svg" width={140} height={140} className="absolute mt-6 ml-44" alt="icon"></Image>
+                <h1 className={satoshiBold.className + " text-cai-500 font-bold text-2xl text-center px-6 py-4"}>{cardTitle}</h1>
+              </div>
+            ))}
+          </div>
+
+          <div className= {satoshiBold.className + " flex flex-col text-center md:max-2xl:text-start md:max-2xl:items-start items-center my-10 lg:max-2xl:my-24 md:max-2xl:pt-36 mr-64 md:max-2xl:pb-16 w-screen"}>
               <h1 className="pt-6 pb-2.5 mb-2.5 md:max-2xl:mb-0 md:max-2xl:py-6 align-bottom md:max-2xl:text-5xl text-3xl">The Officers</h1>
               <OfficerCardTray/>
           </div>
