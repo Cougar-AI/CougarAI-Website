212f141d2a3f91be249748d3eae86389965e0306
tachorzy <tachorzyad@gmail.com>
2023-07-09 23:43:42 -0500

    Changed navbar margins and added scale on hover

--- components/navbar/NavLink.tsx
+++ components/navbar/NavLink.tsx
@@ -8,7 +8,7 @@ interface linkProps {
 const NavLink = (props: linkProps) => {
     return(
         <Link href={props.link}>
-            <h1 className="border-b-3 border-y-0 border-t-0 border-spacing-4 border-white border-none hover:border-solid">{props.text}</h1>
+            <h1 className="border-b-3 border-y-0 border-t-0 border-spacing-4 border-white border-none hover:border-solid hover:scale-105 duration-700">{props.text}</h1>
         </Link>
     )   
 }
