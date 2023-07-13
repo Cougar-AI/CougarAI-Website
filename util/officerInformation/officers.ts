8d093cdcbdb0b153c71e5415d85fefe526bbff55
tachorzy <tachorzyad@gmail.com>
2023-07-13 15:53:14 -0500

    Added comments

--- util/officerInformation/officers.ts
+++ util/officerInformation/officers.ts
@@ -1,4 +1,7 @@
 // unsure if using a map is better in this case. Map<string, { role: string, name: string, linkedIn: string }>
+
+// list of officers will be used to populate the officer cards. Update information here at a later date. Format is as follows:
+// [role, name, linkedIn]
 export const officers: string[][] = [
     ['President', 'Zeel Engineer', 'https://www.linkedin.com/in/zeel-engineer-536265216/'],
     ['Vice President', 'Denzel Wilson', 'https://www.linkedin.com/in/denzel-wilson-2422481a2/'],
