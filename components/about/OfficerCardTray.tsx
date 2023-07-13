b227b886e9a6cb27fa4f9e66e293808111785388
tachorzy <tachorzyad@gmail.com>
2023-07-13 03:22:49 -0500

    Changed width of OfficeCard components, and adjusted OfficerCardTray gap

--- components/about/OfficerCardTray.tsx
+++ components/about/OfficerCardTray.tsx
@@ -3,7 +3,7 @@ import { officers } from '@/util/officerInformation/officers'
 
 const OfficerCardTray = () => {
     return(
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-y-5 md:gap-y-9 md:gap-x-11">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-y-5 md:gap-y-9 md:gap-x-9">
          {officers.map(([officerRole, officerName, officerLinkedIn], index) => (
             <OfficerCard key={index} name={officerName} officerRole={officerRole} officerLinkedIn={officerLinkedIn}></OfficerCard>
          ))}
