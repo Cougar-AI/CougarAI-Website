1904ae2eeb180473cb0a79067c93b16bfeee7536
tachorzy <tachorzyad@gmail.com>
2023-07-17 16:31:13 -0500

    Changed favicon to CAI mascot

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -28,7 +28,7 @@ export default function About() {
 
           {/* Officer section of the page */}
           <div className= {satoshiBold.className + " flex flex-col text-center md:text-start md:items-start items-center my-10 lg:mt-28 mb-16 mr-64 md:pb-16 w-screen"}>
-              <h1 className="pt-6 pb-2.5 mb-2.5 md:mb-0 md:py-6 align-bottom md:text-5xl text-3xl">The Officers</h1>
+              <h1 className="pt-6 pb-2.5 mb-2.5 md:mb-0 md:py-6 align-bottom md:text-5xl text-3xl text-white">The Officers</h1>
               <OfficerCardTray/>
           </div>
 
