ebd9eb00aa0bd0061f05b27eba0485addb406bfe
tachorzy <tachorzyad@gmail.com>
2023-07-16 22:58:45 -0500

    Updated Officer List for Fall 2023

--- util/officerInformation/officers.ts
+++ util/officerInformation/officers.ts
@@ -5,7 +5,11 @@
 export const officers: string[][] = [
     ['President', 'Zeel Engineer', 'https://www.linkedin.com/in/zeel-engineer-536265216/'],
     ['Vice President', 'Denzel Wilson', 'https://www.linkedin.com/in/denzel-wilson-2422481a2/'],
+    ['Secretary', 'Clark Horak', 'https://www.linkedin.com/in/clark-horak-77b158228/'],
+    ['Treasurer', 'Mai Hoang', 'https://www.linkedin.com/in/mai-hoang-760a53255/'],
+    ['Marketing', 'Briana Marcos-Elkamand', 'https://www.linkedin.com/in/briana-marcos-elkamand-521477234/'],
     ['Relations', 'Ashish Job', 'https://www.linkedin.com/in/ashish-job/'],
     ['Events', 'Jonathan Gaucin', 'https://www.linkedin.com/in/jonathangaucin/'],
-    ['Marketing', 'Briana Marcos-Elkamand', 'https://www.linkedin.com/in/briana-marcos-elkamand-521477234/']
+    ['Technical Officer', 'Sagun Kayastha', 'https://www.linkedin.com/in/sagun-kayastha-19516683/'],
+    ['Webmaster', 'Baqir Salim', 'https://www.linkedin.com/in/baqir-salim-887094215/'],
 ]
