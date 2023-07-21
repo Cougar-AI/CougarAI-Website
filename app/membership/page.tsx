85bdd81417d521b06d3f54b85eda826553c79df4
tachorzy <tachorzyad@gmail.com>
2023-07-21 01:45:00 -0500

    Edited membership text

--- app/membership/page.tsx
+++ app/membership/page.tsx
@@ -16,13 +16,12 @@ export default function Membership() {
             <div>
               <h1 className="pb-2 md:pb-5 align-bottom text-4xl md:text-5xl lg:text-6xl">Membership</h1>
               <p className={satoshiMedium.className + " pb-2 md:pb-6 align-bottom text-base md:text-2xl lg:text-3xl leading-normal"}>
-                <span className="">{'Gain'}</span>{' access to exclusive AI workshops, research projects, and rewards through a CougarAI membership.'}
+                <span className="">{'Gain'}</span>{' access to exclusive AI workshops, research projects, and rewards with a CougarAI membership.'}
               </p>
               <p className={satoshiMedium.className + " pb-2 md:pb-6 align-bottom text-base md:text-2xl lg:text-3xl leading-normal"}>
-                {'Through our membership program, you will have more opportunities to explore AI & Data Science in a hands-on environment.'}
+                {'Our membership opens the door to having more opportunities to explore AI & Data Science in a hands-on environment.'}
               </p>
             </div>
-            {/* <InfoCardTray cardTitles={['$10 per semester', '$15 per year']}></InfoCardTray> */}
           </div>
         </section>
 
