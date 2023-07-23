5b22850210c1c54565cda22d5b0bbace1daaba34
tachorzy <tachorzyad@gmail.com>
2023-07-22 23:17:23 -0500

    Added email link to membership page

--- app/membership/page.tsx
+++ app/membership/page.tsx
@@ -25,23 +25,20 @@ export default function Membership() {
 
             <div className="flex flex-col w-full gap-y-4">
               <div className="flex flex-row gap-x-4 h-inherit w-full">
-                <div className="bg-snow rounded-md w-1/2 p-5 px-4">
-                  <h1 className="grid grid-cols-2 text-7xl text-cai-500">$10<span className="text-3xl leading-none text-start">per semester</span></h1>
-                  {/* <Image src="/Icons/noun-algorithm-4814633-FFFAFA.svg" width={175} height={175} className="absolute top-0 ml-40 select-none" alt={` icon`}></Image> */}
-
+                <div className="bg-snow rounded-md w-1/2 py-3 pl-6">
+                  <h1 className="grid grid-cols-2 w-2/3 text-5xl text-cai-500">$10<span className="text-xl leading-none text-start">per semester</span></h1>
                 </div>
-                <div className="bg-misty-rose rounded-md w-1/2 overflow-hidden relative p-5 px-6">
-                  <h1 className="grid grid-cols-2 text-7xl text-cai-500">$15<span className="text-3xl text-start">per year</span></h1>
-
-                  {/* <Image src="/Icons/noun-algorithm-4814633-FFFAFA.svg" width={175} height={175} className="absolute top-0 ml-40 select-none" alt={` icon`}></Image> */}
-
+                <div className="bg-misty-rose rounded-md w-1/2 overflow-hidden relative p-3 pl-6">
+                  <h1 className="grid grid-cols-2 w-2/3 text-5xl text-cai-500">$15<span className="text-xl text-start">per year</span></h1>
                 </div>
               </div>
-              {/* <div className="bg-snow rounded-md h-16 w-full">
-
-              </div> */}
             </div>
           </div>
+
+          <p className={satoshiMedium.className + " pb-2 md:pb-6 align-bottom text-base md:text-2xl lg:text-3xl leading-normal"}>
+              {'Any questions regarding payment can be sent to '} 
+              <a href="mailto:cougaraicontact@gmail.com" className="group duration-700 ease-in-out">cougaraicontact@gmail.com<span className="block max-w-0 group-hover:max-w-max transition-all duration-500 h-0.5 bg-white"/></a>
+          </p>
         </section>
 
         <Image src="/Icons/binary.svg" width="0" height="0" className="lg:absolute lg:right-0 select-none overflow-hidden object-contain lg:object-fill w-full h-[50%] mt-36 lg:w-1/2 lg:h-[47%] md:h-[70%] lg:pl-72 md:top-0 md:mt-[50rem] lg:mt-[30.5rem]" alt="Binary Pattern"></Image>
