c3f9724629a7605d14872a90f648b8960e9a64f5
tachorzy <tachorzyad@gmail.com>
2023-07-29 16:39:33 -0500

    Added MembershipInfoSection component

--- app/membership/page.tsx
+++ app/membership/page.tsx
@@ -1,24 +1,13 @@
 import Image from 'next/image'
+import MembershipInfoSection from '@/components/membership/MembershipInfoSection';
 import { satoshiMedium, satoshiBold } from '@/util/localFonts';
 
 export default function Membership() {
     return (
-      // Deal with this code on the next sprint, too much to include in this sprint.
       <main className="bg-cai-400 h-screen flex flex-col lg:grid lg:grid-cols-2">
         <section className="md:ml-24 lg:ml-32 lg:mt-2 md:max-lg:w-3/5">
           <div className={satoshiBold.className + " flex flex-col text-white text-center md:text-start px-5 md:px-0 md:items-start pt-20 md:pt-64 lg:pt-36 mb-3.5"}>
-            <div>
-              <h1 className="pb-2 md:pb-5 align-bottom text-4xl md:text-5xl lg:text-6xl">Membership</h1>
-              <p className={satoshiMedium.className + " pb-2 md:pb-6 align-bottom text-base md:text-2xl lg:text-3xl leading-normal"}>
-                <span className="">{'Gain'}</span>{' access to exclusive AI workshops, research projects, and rewards with a CougarAI membership.'}
-              </p>
-              <p className={satoshiMedium.className + " pb-2 md:pb-6 align-bottom text-base md:text-2xl lg:text-3xl leading-normal"}>
-                {'Our membership opens the door for all majors to having more opportunities to explore AI & Data Science.'}
-              </p>
-            </div>
-            <p className={satoshiMedium.className + " pb-2 md:pb-6 align-bottom text-base md:text-2xl lg:text-3xl leading-normal mb-2"}>
-                {'Sign up today!'}
-            </p>
+            <MembershipInfoSection/>
             <div className="flex flex-col w-full gap-y-4">
               <div className="flex flex-row gap-x-4 h-inherit w-full">
                 <div className="bg-snow rounded-md w-1/2 py-3 pl-3 lg:pl-6">
---
+++ components/membership/MembershipInfoSection.tsx
@@ -0,0 +1,23 @@
+import { satoshiBold, satoshiMedium } from '@/util/localFonts';
+
+const MembershipInfoSection = () => {
+    return (
+        <div>
+            <div>
+              <h1 className="pb-2 md:pb-5 align-bottom text-4xl md:text-5xl lg:text-6xl">Membership</h1>
+              <p className={satoshiMedium.className + " pb-2 md:pb-6 align-bottom text-base md:text-2xl lg:text-3xl leading-normal"}>
+                <span className="">{'Gain'}</span>{' access to exclusive AI workshops, research projects, and rewards with a CougarAI membership.'}
+              </p>
+              <p className={satoshiMedium.className + " pb-2 md:pb-6 align-bottom text-base md:text-2xl lg:text-3xl leading-normal"}>
+                {'Our membership opens the door for all majors to having more opportunities to explore AI & Data Science.'}
+              </p>
+            </div>
+            <p className={satoshiMedium.className + " pb-2 md:pb-6 align-bottom text-base md:text-2xl lg:text-3xl leading-normal mb-2"}>
+                {'Sign up today!'}
+            </p>
+        </div>
+    )
+
+}
+
+export default MembershipInfoSection;
