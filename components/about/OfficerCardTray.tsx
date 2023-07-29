e002f0503ed55b812e03f6085d019f9f3f481827
tachorzy <tachorzyad@gmail.com>
2023-07-28 21:44:27 -0500

    Updated officer list

--- components/about/OfficerCardTray.tsx
+++ components/about/OfficerCardTray.tsx
@@ -4,8 +4,8 @@ import { officers } from '@/util/officerInformation/officers'
 const OfficerCardTray = () => {
     return(
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-y-5 md:gap-y-9 md:gap-x-9 z-10">
-         {Array.from(officers.entries()).map(([officerRole, officerDetails], index) => (
-            <OfficerCard key={index} name={officerDetails.name} officerRole={officerRole} officerLinkedIn={officerDetails.linkedIn}></OfficerCard>
+         {Array.from(officers.entries()).map(([officerKey, officerDetails], index) => (
+            <OfficerCard key={index} name={officerDetails.name} officerRole={officerDetails.role} officerLinkedIn={officerDetails.linkedIn}></OfficerCard>
          ))}
         </div>
     );
--- util/officerInformation/officers.ts
+++ util/officerInformation/officers.ts
@@ -1,13 +1,14 @@
 // list of officers will be used to populate the officer cards. Update before each semester. (Or whenever an officer position changes)
-
-export const officers = new Map<string, { name: string, linkedIn: string }>([
-    ['President', { name: 'Zeel Engineer', linkedIn: 'https://www.linkedin.com/in/zeel-engineer-536265216/' }],
-    ['Vice President', { name: 'Denzel Wilson', linkedIn: 'https://www.linkedin.com/in/denzel-wilson-2422481a2/' }],
-    ['Secretary', { name: 'Clark Horak', linkedIn: 'https://www.linkedin.com/in/clark-horak-77b158228/' }],
-    ['Treasurer', { name: 'Mai Hoang', linkedIn: 'https://www.linkedin.com/in/mai-hoang-760a53255/' }],
-    ['Marketing', { name: 'Briana Marcos-Elkamand', linkedIn: 'https://www.linkedin.com/in/briana-marcos-elkamand-521477234/' }],
-    ['Relations', { name: 'Ashish Job', linkedIn: 'https://www.linkedin.com/in/ashish-job/' }],
-    ['Events', { name: 'Jonathan Gaucin', linkedIn: 'https://www.linkedin.com/in/jonathangaucin/' }],
-    ['Technical Officer', { name: 'Sagun Kayastha', linkedIn: 'https://www.linkedin.com/in/sagun-kayastha-19516683/' }],
-    ['Webmaster', { name: 'Baqir Salim', linkedIn: 'https://www.linkedin.com/in/baqir-salim-887094215/' }],
+// the role is a redundancy but is here in the case where you have duplicate officer positions (i.e. 2 webmasters, or if the mapped field was a name and you had 2 identical names in the future)
+export const officers = new Map<string, { name: string, role: string, linkedIn: string }>([
+    ['President', { name: 'Zeel Engineer', role: 'President', linkedIn: 'https://www.linkedin.com/in/zeel-engineer-536265216/' }],
+    ['Vice_President', { name: 'Denzel Wilson', role: 'Vice President', linkedIn: 'https://www.linkedin.com/in/denzel-wilson-2422481a2/' }],
+    ['Secretary', { name: 'Clark Horak', role: 'Secretary', linkedIn: 'https://www.linkedin.com/in/clark-horak-77b158228/' }],
+    ['Treasurer', { name: 'Mai Hoang', role: 'Treasurer', linkedIn: 'https://www.linkedin.com/in/mai-hoang-760a53255/' }],
+    ['Marketing', { name: 'Briana Marcos-Elkamand', role: 'Marketing', linkedIn: 'https://www.linkedin.com/in/briana-marcos-elkamand-521477234/' }],
+    ['Relations', { name: 'Ashish Job', role: 'Relations', linkedIn: 'https://www.linkedin.com/in/ashish-job/' }],
+    ['Events', { name: 'Jonathan Gaucin', role: 'Events', linkedIn: 'https://www.linkedin.com/in/jonathangaucin/' }],
+    ['Technical_Officer', { name: 'Sagun Kayastha', role: 'Technical Officer', linkedIn: 'https://www.linkedin.com/in/sagun-kayastha-19516683/' }],
+    ['Webmaster', { name: 'TBD', role: 'Webmaster', linkedIn: 'https://www.linkedin.com' }],
+    ['Webmaster_2', { name: 'Tariq Achor Zyad', role: 'Webmaster', linkedIn: 'https://www.linkedin.com/in/tachorzy' }],
 ])
