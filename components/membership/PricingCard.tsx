fddd610e3ec117f0514db941c56a55688b013352
tachorzy <tachorzyad@gmail.com>
2023-08-20 02:39:32 -0500

    Readded missing chip.svg file

--- components/membership/PricingCard.tsx
+++ components/membership/PricingCard.tsx
@@ -9,7 +9,7 @@ interface PricingCardProps{
 const PricingCard = (props: PricingCardProps) => {
   return (
     <div className="relative flex flex-row gap-x-4 bg-snow rounded-md md:w-1/2 overflow-hidden max-lg:px-7 p-3 lg:pl-6">
-      <Image src="/Icons/chip.svg" width={140} height={140} className="absolute ml-44 lg:ml-40 self-center mt-8 select-none z-0" alt={`Pricing icon`}></Image>
+      <Image src="/icons/chip.svg" width={140} height={140} className="absolute ml-44 lg:ml-40 self-center mt-8 select-none z-0" alt={`Pricing icon`}></Image>
       <h1 className="grid grid-cols-2 lg:w-2/3 text-3xl md:text-4xl lg:text-5xl text-cai-500">
         <span>{props.priceTag}</span>
         <span className={`text-sm md:text-lg lg:text-2xl leading-none text-start pt-1.5 lg:pt-0.5 ${props.priceTag === '$15' ? `lg:pl-0 lg:pr-0.5 pl-0.5` : `pl-1`}`}>{props.subscriptionPlan}</span>
