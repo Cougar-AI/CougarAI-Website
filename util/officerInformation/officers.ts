a01d02a39d66fb236ed591323a30f2439b0f197e
tachorzy <tachorzyad@gmail.com>
2023-09-01 22:42:51 -0500

    Shortened Vice Presidential roles to VP

--- util/officerInformation/officers.ts
+++ util/officerInformation/officers.ts
@@ -2,8 +2,8 @@
 // the role is a redundancy but is here in the case where you have duplicate officer positions (i.e. 2 webmasters, or if the mapped field was a name and you had 2 identical names in the future)
 export const officers = new Map<string, { name: string, role: string, linkedIn: string }>([
     ['President', { name: 'Zeel Engineer', role: 'President', linkedIn: 'https://www.linkedin.com/in/zeel-engineer-536265216/' }],
-    ['Vice_President_Internal', { name: 'Ashish Job', role: 'Vice President Internal', linkedIn: 'https://www.linkedin.com/in/ashish-job/' }],
-    ['Vice_President_External', { name: 'Denzel Wilson', role: 'Vice President External', linkedIn: 'https://www.linkedin.com/in/denzel-wilson-2422481a2/' }],
+    ['Vice_President_Internal', { name: 'Ashish Job', role: 'VP Internal', linkedIn: 'https://www.linkedin.com/in/ashish-job/' }],
+    ['Vice_President_External', { name: 'Denzel Wilson', role: 'VP External', linkedIn: 'https://www.linkedin.com/in/denzel-wilson-2422481a2/' }],
     ['Secretary', { name: 'Clark Horak', role: 'Secretary', linkedIn: 'https://www.linkedin.com/in/clark-horak-77b158228/' }],
     ['Treasurer', { name: 'Mai Hoang', role: 'Treasurer', linkedIn: 'https://www.linkedin.com/in/mai-hoang-760a53255/' }],
     ['Marketing', { name: 'Briana Marcos-Elkamand', role: 'Marketing', linkedIn: 'https://www.linkedin.com/in/briana-marcos-elkamand-521477234/' }],
