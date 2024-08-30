9bfca577c835796b793163792fa51348fbb8a202
GP <GPSins9779@gmail.com>
2024-08-29 19:03:06 -0500

    Added new Officers

--- components/membership/PricingCard.tsx
+++ components/membership/PricingCard.tsx
@@ -12,7 +12,7 @@ const PricingCard = (props: PricingCardProps) => {
       <Image src="/icons/chip.svg" width={140} height={140} className="absolute ml-44 lg:ml-40 self-center mt-8 select-none z-0" alt={`Pricing icon`}></Image>
       <h1 className="grid grid-cols-2 lg:w-2/3 text-3xl md:text-4xl lg:text-5xl text-cai-500">
         <span>{props.priceTag}</span>
-        <span className={`text-sm md:text-lg lg:text-2xl leading-none text-start pt-1.5 lg:pt-0.5 ${props.priceTag === '$15' ? `lg:pl-0 lg:pr-0.5 pl-0.5` : `pl-1`}`}>{props.subscriptionPlan}</span>
+        <span className={`text-sm md:text-lg lg:text-2xl leading-none text-start pt-1.5 lg:pt-0.5 ml-1 ${props.priceTag === '$15' ? `lg:pl-0 lg:pr-0.5 pl-0.5` : `pl-1`}`}>{props.subscriptionPlan}</span>
       </h1>
     </div>
   )
